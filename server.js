7acce3110bafefd131bc3c374338cbb2d8d525da
Giuseppe Fragalà <gmfdev2000@hotmail.com>
2017-08-29 23:21:34 +0200

    Modified serve.js

---
+++ server.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+app.set('views', __dirname + '/');
+
+app.get('/', function(req, res) {
+      res.sendFile(path.join(__dirname + '/index.htm'));
+});
+
+
+app.get('/api/imagesearch/:searchString', function(req, res) {
+	var searchString = req.params.searchString;
+  	res.send(searchString);
+});
+
+
+app.listen(process.env.PORT || 3000)
+console.log("Server is listening you!");
+
