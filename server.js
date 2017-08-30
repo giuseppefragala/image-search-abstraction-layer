4ba96ee6fa5fb32210f38381069bac3c1452e534
Giuseppe Fragalà <gmfdev2000@hotmail.com>
2017-08-30 12:33:02 +0200

    Recent searching works.

--- server.js
+++ server.js
@@ -2,16 +2,22 @@ var express = require('express');
 var app = express();
 var path = require('path');
 var request = require('request');
+var MongoClient = require('mongodb').MongoClient, assert = require('assert');
+var connUrl = process.env.MONGODB_URI;
 
 app.set('views', __dirname + '/');
 
 app.get('/', function(req, res) {
       res.sendFile(path.join(__dirname + '/index.htm'));
 });
-var searchString
-
+var searchString;
+var sysdate;
 app.get('/api/imagesearch/:searchString', function(req, res, next) {
   searchString = req.params.searchString;
+  sysdate=new Date().toISOString();
+  var arr = []
+  arr.push({term:searchString, when: sysdate})
+  write((arr))
     next()
   //res.redirect('https://www.googleapis.com/customsearch/v1?q=' + searchString + '&cx=' + process.env.cx + '&key=' + process.env.key + '&searchType=image')
 
@@ -33,6 +39,57 @@ app.get('/api/imagesearch/:searchString', function(req, res, next) {
   });
 
 
+app.get('/api/latest/imagesearch/', function(req, res, next) {
+    //res.send(read())
+  console.log(read(res))
+  //res.end()
+  });
 
 app.listen(process.env.PORT || 3000)
 console.log("Server is listening you!");
+
+function write (object){
+// --------- INSERT A SINGLE DOCUMENT OR AN ARRAY OF DOCUMENT INTO A COLLECTION -------------------------------------------------
+MongoClient.connect(connUrl,function (err, db) {
+if(err) return err;
+  // Get the documents collection
+  var collection = db.collection('imagesearch');
+  // Insert the documents
+  collection.insert(object, function (err, result) {
+  db.close();
+  });
+});
+// --------- INSERT A SINGLE DOCUMENT OR AN ARRAY OF DOCUMENT INTO A COLLECTION --------------------------------------------------
+
+};
+
+
+
+var findDocuments = function(db, callback) {
+  // Get the documents collection
+  var collection = db.collection('imagesearch');
+  // Find some documents
+  collection.find({}).toArray(function(err, docs) {
+    assert.equal(err, null);
+    console.log("Found the following records");
+    console.log(docs)
+    callback(docs);
+  });
+}
+
+
+function read(response){
+MongoClient.connect(connUrl,function (err, db) {
+  if(err) return err;
+    // Get the documents collection
+    var collection = db.collection('imagesearch');
+    var res;
+    // Perform a simple find and return all the documents
+    collection.find({},{_id:0}).toArray(function(err, docs) { 
+        res = JSON.stringify(docs);
+        response.send(res)
+        db.close();
+    });
+});
+
+};
