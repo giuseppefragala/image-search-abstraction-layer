277a795f32c3cc45fdf019258ccb46011b502e7b
Giuseppe Fragalà <gmfdev2000@hotmail.com>
2017-08-30 10:18:36 +0200

    Added request module. Now image searching works.

--- server.js
+++ server.js
@@ -1,18 +1,37 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var request = require('request');
 
 app.set('views', __dirname + '/');
 
 app.get('/', function(req, res) {
       res.sendFile(path.join(__dirname + '/index.htm'));
 });
+var searchString
 
+app.get('/api/imagesearch/:searchString', function(req, res, next) {
+  searchString = req.params.searchString;
+    next()
+  //res.redirect('https://www.googleapis.com/customsearch/v1?q=' + searchString + '&cx=' + process.env.cx + '&key=' + process.env.key + '&searchType=image')
 
-app.get('/api/imagesearch/:searchString', function(req, res) {
-	var searchString = req.params.searchString;
-  	res.send(searchString);
-});
+}, function (req, res) {
+      request('https://www.googleapis.com/customsearch/v1?q=' + searchString + '&cx=' + process.env.cx + '&key=' + process.env.key + '&searchType=image',function(err,result,body) {
+      var images = [];
+          var data = JSON.parse(body);
+          data.items.forEach(function(val) {
+            var obj = {
+              image: val.link,
+              text: val.snippet,
+              source: val.link
+            }
+            images.push(obj);
+          })
+          res.send(images);
+      })  
+  
+  });
+
 
 
 app.listen(process.env.PORT || 3000)
